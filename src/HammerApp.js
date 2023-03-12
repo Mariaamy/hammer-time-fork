da0d860e8de86f0c5fa329c7f5ea8196dc3d3437
Mariaamy <mariaamy@stud.ntnu.no>
2023-03-12 15:26:26 +0100

    Add logout page

--- src/HammerApp.js
+++ src/HammerApp.js
@@ -9,6 +9,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Userpage from './pages/Userpage';
 import Adminpage from './pages/Adminpage';
+import Logout from './pages/Logout';
 
 
 // Route access
@@ -54,6 +55,8 @@ const HammerApp = () => {
                         <Route path="/logout" element={<Logout/>} />
                     </Route>
                     */}
+
+                    <Route path="/logout" element={<Logout/>} />
 
                     {/* Accessible by authenticated admin users */}
 
