2c5079dff3e7b2cc0b4e33eaf34a01f8eb02d0ed
CedricThode <cedricat@stud.ntnu.no>
2023-03-17 11:03:44 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,10 +6,10 @@ function Login() {
     <div className="section--login--container">
         <form method='POST'>
         <h1>Login</h1>
-            <label for="email">E-mail</label>
-            <input type="email" id="email" name="email"/>
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password"/>
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" placeholder='Email'/>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" placeholder='Password'/>
             <button type="submit" id="login">Login</button>
             <p>Or register an account <a href="/register">here</a></p>
         </form>
