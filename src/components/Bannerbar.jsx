50ccca879b9ed9d186106bb6f325a831bf8430ae
Jason <jimanproductions@gmail.com>
2023-03-27 20:17:32 +0200

    updated icons, updated colors

--- src/components/Bannerbar.jsx
+++ src/components/Bannerbar.jsx
@@ -4,7 +4,7 @@ function Bannerbar(props) {
   return (
   <div className='bannerItem'>
     <img src={props.image} alt="warning icon"/>
-    <div className='banner'>
+    <div className={`banner ${props.className}`}>
     <p className='bannerTitle'>{props.title}</p>
     <p className='bannerBody'>{props.amount}</p>
     </div>
