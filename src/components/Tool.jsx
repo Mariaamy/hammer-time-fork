be69dd279a19ec1935ab7746083de839528f17d2
Mariaamy <mariaamy@stud.ntnu.no>
2023-03-31 09:27:00 +0200

    Fetch tool data

--- src/components/Tool.jsx
+++ src/components/Tool.jsx
@@ -1,26 +1,64 @@
 import ToolCard from "./Tool/ToolCard";
 import ToolProfile from "./Tool/ToolProfile";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Tool(props) {
     const [data, setData] = useState({});
+
+    let { toolID } = useParams();
+
+    const [tool, setTool] = useState();
+
+  
+
+
 
     useEffect(() => {
         if(props.data) {
             setData(props.data);
         }else if(props.toolID) {
             // Fetch API data here
+
+
+            //FECTHES DATA BUT FAILS TO LOAD? ASYNC AWAIT?
+
+
+            const fetchTool = async () => {
+                try {
+                const res = await axios.get(`/api/tools/${toolID}`)
+                setTool(res.data[0])
+
+               
+                } catch(error) {
+                    console.log(error)
+                }
+          
+            }
+
+        
+            fetchTool()
+        
+            
+
+            
             setData(
                 {
                     _id: "test",
-                    title: "Hammer",
-                    information: "lol",
+                    name: "Hammer",
+                    information: "ja",
                     availability: "Yes?"
                 }
             )
         }
     }, [props.toolID, props.data]);
 
+
+    setTimeout(() => {
+    console.log(tool.name)
+    }, 1000)
+    
     return (props.variant==="card" && <ToolCard data={data}/>) || <ToolProfile data={data}/>;
 }
   
