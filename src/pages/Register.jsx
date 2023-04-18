987726ecd356944ab90cae1c62da16354aa8e4af
CedricThode <cedricat@stud.ntnu.no>
2023-04-19 01:59:48 +0200

    Added name and surname to register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,8 +6,13 @@ function Register() {
     <div className="section--login--container">
         <form method='POST'>
         <h1>Register a new user here!</h1>
+            <label for="name">Name:</label>
+            <input type="name" id="name" name="name" placeholder='Name'/>
+            <label for="surname:">Surname:</label>
+            <input type="surname:" id="surname:" name="surname:" placeholder='Surname:'/>
             <label for="email">E-mail:</label>
             <input type="email" id="email" name="email" placeholder='Email'/>
+            
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" placeholder='Password'/>
             <button type="submit" id="register">Register</button>
