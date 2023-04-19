8a49d7ab24bb20b614f2442ac3b80595a83fbc5a
Ola Hulleberg <ola.hulleberg@gmail.com>
2023-04-19 17:54:17 +0200

    Add support for other profiles

--- src/pages/Userpage.jsx
+++ src/pages/Userpage.jsx
@@ -1,20 +1,28 @@
 import UserProfile from "../components/User/UserProfile";
 import hAPI from "../api/hAPI";
 import { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
 import AuthContext from "../providers/AuthProvider";
 
 function Userpage() {
+  const { userID } = useParams();
   const authContext = useContext(AuthContext);
   const [user, setUser] = useState([]);
 
   useEffect(() => {
-    const user = authContext.getUser();
-    if (user) {
-      hAPI.users.getUser(user.sub).then((response) => {
+    if (userID) {
+      hAPI.users.getUser(userID).then((response) => {
         setUser(response.data);
       });
+    } else {
+      const user = authContext.getUser();
+      if (user) {
+        hAPI.users.getUser(user.sub).then((response) => {
+          setUser(response.data);
+        });
+      }
     }
-  }, [authContext]);
+  }, [authContext, userID]);
 
   return (
     <>
